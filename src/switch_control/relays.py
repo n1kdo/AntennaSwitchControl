15163768e7a283478130ac7736d861b95bc681d2
Jeff Otterson <n1kdo@users.noreply.github.com>
2025-03-24 13:24:11 -0400

    # Conflicts: #	src/switch_control/http_server.py

--- src/switch_control/relays.py
+++ src/switch_control/relays.py
@@ -4,7 +4,7 @@
 
 __author__ = 'J. B. Otterson'
 __copyright__ = """
-Copyright 2024, J. B. Otterson N1KDO.
+Copyright 2024, 2025, J. B. Otterson N1KDO.
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
   1. Redistributions of source code must retain the above copyright notice, 
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from utils import upython
 import micro_logging as logging
@@ -36,31 +36,31 @@ else:
 NUM_RELAYS = 8
 
 a_pins = [
-    machine.Pin(13, machine.Pin.OUT, value=0),  # GP9 pin 17
-    machine.Pin(12, machine.Pin.OUT, value=0),  # GP9 pin 16
-    machine.Pin(11, machine.Pin.OUT, value=0),  # GP9 pin 5
-    machine.Pin(10, machine.Pin.OUT, value=0),  # GP9 pin 14
-    machine.Pin(9, machine.Pin.OUT, value=0),  # GP9 pin 12
-    machine.Pin(8, machine.Pin.OUT, value=0),  # GP9 pin 11
-    machine.Pin(7, machine.Pin.OUT, value=0),  # GP9 pin 10
-    machine.Pin(6, machine.Pin.OUT, value=0),  # GP9 pin 9
+    machine.Pin(15, machine.Pin.OUT, value=0),  # A1 GP15 pin 20
+    machine.Pin(14, machine.Pin.OUT, value=0),  # A2 GP14 pin 19
+    machine.Pin(13, machine.Pin.OUT, value=0),  # A3 GP13 pin 17
+    machine.Pin(12, machine.Pin.OUT, value=0),  # A4 GP12 pin 16
+    machine.Pin(11, machine.Pin.OUT, value=0),  # A5 GP11 pin 15
+    machine.Pin(10, machine.Pin.OUT, value=0),  # A6 GP10 pin 14
+    machine.Pin(9, machine.Pin.OUT, value=0),  # A7 GP9 pin 12
+    machine.Pin(8, machine.Pin.OUT, value=0),  # A8 GP8 pin 11
 ]
 
 # define pins for antenna switch port b
 b_pins = [
-    machine.Pin(27, machine.Pin.OUT, value=0),  # GP27 pin 32
-    machine.Pin(26, machine.Pin.OUT, value=0),  # GP26 pin 31
-    machine.Pin(21, machine.Pin.OUT, value=0),  # GP21 pin 27
-    machine.Pin(20, machine.Pin.OUT, value=0),  # GP20 pin 26
-    machine.Pin(19, machine.Pin.OUT, value=0),  # GP19 pin 25
-    machine.Pin(18, machine.Pin.OUT, value=0),  # GP18 pin 24
-    machine.Pin(17, machine.Pin.OUT, value=0),  # GP17 pin 22
-    machine.Pin(16, machine.Pin.OUT, value=0),  # GP16 pin 21
+    machine.Pin(7, machine.Pin.OUT, value=0),  # B1 GP7 pin 10
+    machine.Pin(6, machine.Pin.OUT, value=0),  # B2 GP6 pin 9
+    machine.Pin(5, machine.Pin.OUT, value=0),  # B3 GP5 pin 7
+    machine.Pin(4, machine.Pin.OUT, value=0),  # B4 GP4 pin 6
+    machine.Pin(3, machine.Pin.OUT, value=0),  # B4 GP3 pin 5
+    machine.Pin(2, machine.Pin.OUT, value=0),  # B6 GP2 pin 4
+    machine.Pin(1, machine.Pin.OUT, value=0),  # B7 GP1 pin 2
+    machine.Pin(0, machine.Pin.OUT, value=0),  # B8 GP0 pin 1
 ]
 
 
 def set_port(radio: int, port_selected: int):
-    if 0 <= port_selected < NUM_RELAYS:
+    if 0 <= port_selected <= NUM_RELAYS:
         pins = None
         if radio == 1:
             pins = a_pins
