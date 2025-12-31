c29a38390ca9a6ba18260d423b3647ec0c53f14f
n1kdo <otterson@gmail.com>
2025-12-31 13:38:13 -0500

    UDP broadcast, packing correctness

--- src/switch_control/udp_messages.py
+++ src/switch_control/udp_messages.py
@@ -19,14 +19,11 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-__version__ = '0.0.2'  # 2025-11-05
+__version__ = '0.0.3'  # 2025-12-31
 
 from utils import upython
 import asyncio
-if upython:
-    import micro_logging as logging
-else:
-    import logging
+import micro_logging as logging
 import socket
 from struct import calcsize, pack_into
 
@@ -45,12 +42,20 @@ STATUS_BROADCAST_SIZE = calcsize(STATUS_BROADCAST_FMT)
 
 
 def calculate_broadcast_address(ip_address, netmask):
-    # calculate the subnet's broadcast address using ip_address and netmask
+    # validate quick sanity of inputs
+    try:
+        ip_parts = [int(x) for x in ip_address.split('.')]
+        mask_parts = [int(x) for x in netmask.split('.')]
+        if len(ip_parts) != 4 or len(mask_parts) != 4:
+            raise ValueError('invalid ip or netmask format')
+    except Exception as ex:
+        logging.error(f'calculate_broadcast_address: invalid input: {ex}', 'udp_messages:calculate_broadcast_address')
+        raise
+
     ip_int = sum([int(x) << 8 * i for i, x in enumerate(reversed(ip_address.split('.')))])
     mask_int = sum([int(x) << 8 * i for i, x in enumerate(reversed(netmask.split('.')))])
     mask_mask = mask_int ^ 0xffffffff
     bcast_int = ip_int | mask_mask
-    # TODO make this into bytes, not str -- maybe
     bcast_addr = ".".join(map(str, [
         ((bcast_int >> 24) & 0xff),
         ((bcast_int >> 16) & 0xff),
@@ -58,6 +63,15 @@ def calculate_broadcast_address(ip_address, netmask):
         (bcast_int & 0xff),
     ]))
     return bcast_addr
+
+def _fit_and_encode(s, length):
+    """Encode string to bytes, truncate or pad with NUL to length."""
+    if s is None:
+        s = ''
+    b = s.encode('utf-8', 'ignore')[:length]
+    if len(b) < length:
+        b = b + (b'\x00' * (length - len(b)))
+    return b
 
 
 class SendBroadcasts:
@@ -67,6 +81,7 @@ class SendBroadcasts:
 
     def __init__(self, target_ip, target_port, config: dict, antennas_selected):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sockaddr = socket.getaddrinfo(target_ip, target_port)[0][-1]
         self.config = config
@@ -88,29 +103,33 @@ class SendBroadcasts:
         buf = self.buf
         sleep = asyncio.sleep
         while self.run:
-            pack_into(STATUS_BROADCAST_FMT, buf, 0,
-                  antennas_selected[0],
-                      antennas_selected[1],
-                      radio_names[0],
-                      radio_names[1],
-                      antenna_names[0],
-                      antenna_names[1],
-                      antenna_names[2],
-                      antenna_names[3],
-                      antenna_names[4],
-                      antenna_names[5],
-                      antenna_names[6],
-                      antenna_names[7],
-                      antenna_bands[0],
-                      antenna_bands[1],
-                      antenna_bands[2],
-                      antenna_bands[3],
-                      antenna_bands[4],
-                      antenna_bands[5],
-                      antenna_bands[6],
-                      antenna_bands[7],
-                      hostname)
-            self.send(buf)
+            try:
+                pack_into(STATUS_BROADCAST_FMT, buf, 0,
+                          int(antennas_selected[0]),
+                          int(antennas_selected[1]),
+                          _fit_and_encode(radio_names[0] if len(radio_names) > 0 else '', 16),
+                          _fit_and_encode(radio_names[1] if len(radio_names) > 1 else '', 16),
+                          _fit_and_encode(antenna_names[0] if len(antenna_names) > 0 else '', 20),
+                          _fit_and_encode(antenna_names[1] if len(antenna_names) > 1 else '', 20),
+                          _fit_and_encode(antenna_names[2] if len(antenna_names) > 2 else '', 20),
+                          _fit_and_encode(antenna_names[3] if len(antenna_names) > 3 else '', 20),
+                          _fit_and_encode(antenna_names[4] if len(antenna_names) > 4 else '', 20),
+                          _fit_and_encode(antenna_names[5] if len(antenna_names) > 5 else '', 20),
+                          _fit_and_encode(antenna_names[6] if len(antenna_names) > 6 else '', 20),
+                          _fit_and_encode(antenna_names[7] if len(antenna_names) > 7 else '', 20),
+                          int(antenna_bands[0]) if len(antenna_bands) > 0 else 0,
+                          int(antenna_bands[1]) if len(antenna_bands) > 1 else 0,
+                          int(antenna_bands[2]) if len(antenna_bands) > 2 else 0,
+                          int(antenna_bands[3]) if len(antenna_bands) > 3 else 0,
+                          int(antenna_bands[4]) if len(antenna_bands) > 4 else 0,
+                          int(antenna_bands[5]) if len(antenna_bands) > 5 else 0,
+                          int(antenna_bands[6]) if len(antenna_bands) > 6 else 0,
+                          int(antenna_bands[7]) if len(antenna_bands) > 7 else 0,
+                          _fit_and_encode(hostname, 64))
+                # send the raw buffer
+                self.send(buf)
+            except Exception as ex:
+                logging.error(f'Broadcast pack/send failed: {ex}', 'udp_messages:send_datagrams')
             await sleep(1.0)
 
     def stop(self):
